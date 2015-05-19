6d805572af18426162745c982afa29c3fc75c91e
Fabian <irgendwas@gmail.com>
2015-05-19 13:09:55 +0200

    VOC-30 - UC "Unit Testing"

---
+++ app/src/main/java/com/example/fabianfleischer/thevocabmaster/library/Word.java
@@ -0,0 +1,35 @@
+package com.example.fabianfleischer.thevocabmaster.library;
+
+/**
+ * Created by fabian.fleischer on 14.05.2015.
+ */
+public class Word {
+    private long id;
+    private String deutsch;
+    private String englisch;
+
+    public long getID(){
+        return id;
+    }
+
+    public void setID(long id){
+        this.id=id;
+    }
+
+    public String getGerman(){
+        return deutsch;
+    }
+
+    public String gerEnglisch() {
+        return englisch;
+    }
+
+    public void setDeutsch(String deutsch){
+        this.deutsch=deutsch;
+    }
+
+    public void setEnglisch(String englisch){
+        this.englisch=englisch;
+    }
+
+}
