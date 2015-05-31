1dd89600401be2faaf8b4649f77f1017d7a9779d
Fabi153 <fabianschorb93@aol.com>
2015-05-31 14:51:33 +0200

    OnClick Register Button geändert

--- app/src/main/java/com/example/fabianfleischer/thevocabmaster/RegisterActivity.java
+++ app/src/main/java/com/example/fabianfleischer/thevocabmaster/RegisterActivity.java
@@ -84,7 +84,7 @@ public class RegisterActivity extends Activity {
             Toast toast = Toast.makeText(context, "Register Success", duration);
             toast.show();
             Intent i = new Intent(getApplicationContext(),
-                    UebenActivity.class);
+                    MenuActivity.class);
             startActivity(i);
             finish();
         }
