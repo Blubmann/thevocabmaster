80ceee3eb954ce030ecd8223e0752e280c0c6de3
JohannesResch94 <johannes-resch@t-online.de>
2015-06-01 10:52:58 +0200

    Statistics fertig implementiert

--- app/src/main/java/com/example/fabianfleischer/thevocabmaster/StatisticActivity.java
+++ app/src/main/java/com/example/fabianfleischer/thevocabmaster/StatisticActivity.java
@@ -19,38 +19,36 @@ import android.widget.Toast;
 import com.example.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class StatisticActivity extends ListActivity {
 
+    private DatabaseDataSource dataSource;
     public ListView vocList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
-        //  dataSource= new DatabaseDataSource(this);
-        //  dataSource.open();
-        vocList = (ListView) findViewById(android.R.id.list);
+          dataSource= new DatabaseDataSource(this);
+          dataSource.open();
+        List<Word> allWords= dataSource.getAllWords();
 
-        String[] allWords = {"Fleischer", "Chillt", "sein", "Leben", "ziemlich", "hart", "Nesaf", "dsgsdgg", "sdafsdf", "ghjghj", "öoiöiojhkl", "vqweweqer"};
+        vocList = (ListView) findViewById(android.R.id.list);
         fillListStatisticList(allWords);
     }
-    public void fillListStatisticList(String[] allWords){
-        ArrayList meineListe = new ArrayList();
-        for (int i=0; i< allWords.length;i++){
-            meineListe.add(allWords[i]);
-        }
+    public void fillListStatisticList(List<Word> allWords){
+
+        ArrayList meineListe = (ArrayList) allWords;
         ArrayAdapter listenAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, meineListe);
         vocList.setAdapter(listenAdapter);
         vocList.setOnItemClickListener( new android.widget.AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
-
-                String item = ((TextView) view).getText().toString();
-                //hier müssen Daten abhängig von Geklickten namen aus derr Daten bank gelesen werden
-                showAlertBox(item, "Wort in Englisch",1,1);
+                Word choosenWord = (Word) parent.getAdapter().getItem(position);
+                showAlertBox(choosenWord);
 
 
             }
@@ -58,13 +56,10 @@ public class StatisticActivity extends ListActivity {
 
     }
 
-    public void showAlertBox(String wort_de, String wort_en, int anz_falsch, int anz_richtig){
-        String hallo[][]= new String[1][1];
-
-        hallo[0][0] = "Hallo";
+    public void showAlertBox(Word choosenWord){
 
         AlertDialog.Builder builder1 = new AlertDialog.Builder(StatisticActivity.this);
-        String msg = "Deine Statistik fuer das Wort " + wort_de + " / " + wort_en + " :" + "\n Anzahl korrekter Eingaben: " + anz_richtig + "\n Anzahl falscher Eingaben: " + anz_falsch;
+        String msg = "Deine Statistik fuer das Wort " + choosenWord.getGerman() + " / " + choosenWord.gerEnglisch() + " :" + "\n Anzahl korrekter Eingaben: " + choosenWord.getRichtig() + "\n Anzahl falscher Eingaben: " + choosenWord.getFalsch();
         builder1.setMessage(msg);
         builder1.setCancelable(true);
         builder1.setNeutralButton("Okay",
